2b701b2133aeacfa5528c4d9d0ef4aa7cc88b7f0
Dennis Lin <pioneerlike@gmail.com>
2016-05-21 04:26:34 -0700

    routes: init structure, 404 handling

--- server/server.js
+++ server/server.js
@@ -2,10 +2,30 @@
 
 var express = require('express');
 var port = process.env.PORT || 3000;
-var app = express();
-var models = require("./models");
 
-app.use(express.static(__dirname + "/../public"));
+var api_router = require('./routes/api-router')
+var serve_static = express.static(__dirname + "/../public");
+
+var app = express();
+app.use('/api/v1', api_router);
+app.use('/', serve_static);
+
+// 404 Handling
+app.use(function(req, res, next){
+  res.status(404);
+
+  // respond with html page
+  if (req.accepts('html')) {
+    res.sendFile(__dirname + '/static/404.html');
+    return;
+  }
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.send({ error: 'Not found' });
+    return;
+  }
+});
 
 app.listen(port);
 
