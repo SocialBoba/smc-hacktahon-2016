564257433d5eb2f8119780dbac2e9d0b7d4cc674
Dennis Lin <pioneerlike@gmail.com>
2016-05-20 22:35:12 -0700

    server init with API docs

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 "use strict";
 var express = require('express');
-var port = process.env.PORT || 3000;;
+var port = process.env.PORT || 3000;
 var app = express();
  
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/../public"));
 
 app.listen(port);
 
