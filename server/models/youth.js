47bd707e32b2b6ed16e20e5620ec80a50a03baa6
Dennis Lin <pioneerlike@gmail.com>
2016-05-21 00:48:03 -0700

    db models (sequelize) init

---
+++ server/models/youth.js
@@ -0,0 +1,22 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Youth = sequelize.define('Youth', {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    city: DataTypes.STRING,
+    state: DataTypes.STRING,
+    postal: DataTypes.INTEGER,
+    linkedin_url: DataTypes.TEXT,
+    youtube_url: DataTypes.TEXT,
+    intro: DataTypes.TEXT,
+    org: DataTypes.STRING,
+    profile_pic_url: DataTypes.TEXT
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Youth;
+};
--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 "use strict";
+
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
- 
+var models = require("./models");
 
 app.use(express.static(__dirname + "/../public"));
 
 app.listen(port);
 
 console.log("listening on " + port);
-
